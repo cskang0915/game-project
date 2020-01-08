0b4b8d4f9958dd2b40b5ab0f2d275d34de9f25aa
ekcs0915 <ekcs0915@gmail.com>
2020-01-07 19:28:15 -0800

    created index.js to display information

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+let express = require('express')
+
+let app = express()
+
+app.get('/', (req, res)=>{
+	res.send('Beginning of the project')
+})
+
+app.listen(9000, ()=>{
+	console.log('Listening on port 9000')
+})
